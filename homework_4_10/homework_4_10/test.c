78800a093b1e22fb6c97c24dc20ea8da2a6eeff4
gascsd <3311922913@qq.com>
2022-05-06 01:12:49 +0800

    比特4月10日作业

---
+++ homework_4_10/homework_4_10/test.c
@@ -0,0 +1,58 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//BC131 - kiki学程序设计基础
+//#include<stdio.h>
+//int main()
+//{
+//    printf("printf(\"Hello world!\\n\");\n");
+//    printf("cout << \"Hello world!\" << endl;");
+//    return 0;
+//}
+
+
+
+//BC123-小乐乐找最大数		
+//#include<stdio.h>
+//int Max(int a, int b)
+//{
+//    return a > b ? a : b;
+//}
+//int main()
+//{
+//    int a, b, c, d = 0;
+//    int max = 0;
+//    scanf("%d%d%d%d", &a, &b, &c, &d);
+//    max = a;
+//    max = Max(max, b);
+//    max = Max(max, c);
+//    max = Max(max, d);
+//    printf("%d", max);
+//    return 0;
+//}
+
+
+
+//BC27-计算球体的体积
+//#define PI 3.1415926
+//#include<stdio.h>
+//int main()
+//{
+//    double r = 0, V = 0;
+//    scanf("%lf", &r);
+//    V = (4 * PI * r * r * r) / 3;
+//    printf("%.3lf", V);
+//    return 0;
+//}
+
+
+//BC25-计算体重指数
+//#include<stdio.h>
+//int main()
+//{
+//    float BIM = 0;
+//    int high = 0, wight = 0;
+//    scanf("%d%d", &wight, &high);
+//    BIM = wight / ((high / 100.0) * (high / 100.0));
+//    printf("%.2f", BIM);
+//    return 0;
+//}
