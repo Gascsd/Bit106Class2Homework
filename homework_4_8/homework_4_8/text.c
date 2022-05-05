cd14b5bca6a0504588bb0c0a77a641d92a23d351
gascsd <3311922913@qq.com>
2022-05-05 14:37:49 +0800

    比特4月8号作业

---
+++ homework_4_8/homework_4_8/text.c
@@ -0,0 +1,58 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+//BC18-计算带余除法
+//#include<stdio.h>
+//int main()
+//{
+//    int a, b, c, d;
+//    scanf("%d%d", &a, &b);
+//    c = a / b;
+//    d = a - b * c;
+//    printf("%d %d\n", c, d);
+//}
+
+//BC13-ASCII码
+//#include<stdio.h>
+//int main()
+//{
+//    char ch[] = { 73,32,99,97,110,32,100,111,32,105,116,33 };
+//    int i = 0;
+//    for (i = 0; i < sizeof(ch); i++)
+//    {
+//        printf("%c", ch[i]);
+//    }
+//    return 0;
+//}
+
+
+//BC14-出生日期输入输出
+//#include<stdio.h>
+//int main()
+//{
+//	int year = 0, month = 0, day = 0;
+//	scanf("%4d%2d%2d", &year, &month, &day);
+//	printf("year=%4d\nmonth=%02d\ndate=%02d\n", year, month, day);
+//	return 0;
+//}
+
+
+//BC11-学生基本信息输入输出
+//#include<stdio.h>
+//int main()
+//{
+//    int s = 0;
+//    float C, math, english = 0;
+//    scanf("%d;%f,%f,%f", &s, &C, &math, &english);
+//    printf("The each subject score of No. %d is %.2f, %.2f, %.2f.", s, C, math, english);
+//    return 0;
+//}
+
+
+//BC9 - printf函数的返回值
+//#include<stdio.h>
+//int main()
+//{
+//    printf("\n%d", printf("Hello world!"));
+//    return 0;
+//}
