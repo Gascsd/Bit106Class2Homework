cfc84c14c8fe2f85c8b7af94326ff52886fba217
gascsd <3311922913@qq.com>
2022-05-08 19:56:10 +0800

    比特4月15日作业

--- homework_4_15/homework_4_15/test.c
+++ homework_4_15/homework_4_15/test.c
@@ -45,7 +45,141 @@
 
 
 //求10 个整数中最大值
+//#include<stdio.h>
+//int main()
+//{
+//	int max = 0;
+//	int n = 0;
+//	int i = 0;
+//	scanf("%d", &max);
+//	for (i = 0; i < 9; i++)
+//	{
+//		scanf("%d", &n);
+//		if (n > max)
+//			max = n;
+//	}
+//	printf("the max number is: %d", max);
+//	return 0;
+//}
+
+
+
 //在屏幕上输出9*9乘法口诀表
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (i = 1; i <= 9; i++)
+//	{
+//		for (j = 1; j <= i; j++)
+//		{
+//			printf("%d*%d=%2d  ", j, i, i * j);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+
+
+
+
 //完成上课的猜数字游戏
+//#include<stdio.h>
+//
+//void menu()
+//{
+//	printf("********************************\n");
+//	printf("*******1. play *****************\n");
+//	printf("*******2. exit *****************\n");
+//	printf("********************************\n");
+//}
+//
+//void game()
+//{
+//	int n;
+//	int ret = rand() % 100 + 1;
+//	//printf("%d\n", ret);
+//	while (1)
+//	{
+//		printf("please guess a number\n");
+//		scanf("%d", &n);
+//		if (n > ret)
+//		{
+//			printf("guess bigger than the number\n");
+//		}
+//		else if (n < ret)
+//		{
+//			printf("guess smaller than the number\n");
+//		}
+//		else
+//		{
+//			printf("Congratulations, you are right\n");
+//			break;
+//		}
+//	}
+//}
+//
+//int main()
+//{
+//	int input = 0;
+//	srand((unsigned int)time(NULL));
+//	do
+//	{
+//		menu();
+//		printf("please choose(0/1)");
+//		scanf("%d", &input);
+//		switch (input)
+//		{
+//		case 1:
+//			printf("play game\n");
+//			game();
+//			break;
+//		case 0:
+//			printf("exit game\n");
+//			break;
+//		default:
+//			printf("input error,please input again\n");
+//		}
+//		
+//	} while (input);
+//	return 0;
+//}
+
+
+
+
+
+
 //二分查找编写代码在一个整形有序数组中查找具体的某个数
 //要求：找到了就打印数字所在的下标，找不到则输出：找不到。
+//#include<stdio.h>
+//int main()
+//{
+//	int n;
+//	int ch[10] = { 10,11,12,13,14,15,16,17,18,19 };
+//	int sz = sizeof(ch) / sizeof(ch[0]);
+//	int left = 0;
+//	int right = sz - 1;
+//	scanf("%d", &n);
+//	while (left <= right)
+//	{
+//		int mid = (left + right) / 2;
+//		if (ch[mid] > n)
+//		{
+//			right = mid - 1;
+//		}
+//		else if (ch[mid] < n)
+//		{
+//			left = mid + 1;
+//		}
+//		else if(ch[mid]==n)
+//		{
+//			printf("找到了，下标为%d\n", mid);
+//			return 0;
+//		}
+//	}
+//	printf("找不到\n");
+//	return 0;
+//}
