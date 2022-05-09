f5fd114ef56786ad7c3f6e9ed626105b7e665e4e
gascsd <3311922913@qq.com>
2022-05-09 20:30:29 +0800

    比特4月17作业

---
+++ homework_4_17/homework_4_17/test.c
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//实现一个函数来交换两个整数的内容。
+//#include<stdio.h>
+//void Swap(int* x, int* y)
+//{
+//	int tmp = 0;
+//	tmp = *x;
+//	*x = *y;
+//	*y = tmp;
+//}
+//
+//int main()
+//{
+//	int a = 5;
+//	int b = 3;
+//	printf("a=%d,b=%d\n", a, b);
+//	Swap(&a, &b);
+//	printf("a=%d,b=%d\n", a, b);
+//	return 0;
+//}
+
+
+//实现一个函数，打印乘法口诀表，口诀表的行数和列数自己指定
+//如：输入9，输出9 * 9口诀表，输出12，输出12 * 12的乘法口诀表。
+//#include<stdio.h>
+//void print(int n)
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (i = 1; i <= n; i++)
+//	{
+//		for (j = 1; j <= i; j++)
+//		{
+//			printf("%-2d*%-2d=%-4d  ", j, i, j * i);
+//		}
+//		printf("\n");
+//	}
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	print(n);
+//	return 0;
+//}
