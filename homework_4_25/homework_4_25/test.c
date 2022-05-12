71fa9c248c815205ca258e59d15611e6e97e9658
gascsd <3311922913@qq.com>
2022-05-12 19:22:51 +0800

    作业更正

--- homework_4_25/homework_4_25/test.c
+++ homework_4_25/homework_4_25/test.c
@@ -2,14 +2,14 @@
 
 //实现一个对整形数组的冒泡排序
 //#include<stdio.h>
-//void bubble_sort(int* arr)
+//void bubble_sort(int* arr,int sz)
 //{
 //	int tmp = 0;
 //	int i = 0;
 //	int count = 0;
-//	while (count < 10)
+//	while (count < sz)
 //	{
-//		for (i = 0; i < 10 - count - 1; i++)
+//		for (i = 0; i < sz - count - 1; i++)
 //		{
 //			if (*(arr+i) > *(arr+i + 1))
 //			{
@@ -25,7 +25,8 @@
 //{
 //	int i = 0;
 //	int ch[10] = { 9,8,7,6,5,4,3,2,1,0 };
-//	bubble_sort(ch);
+//	int sz = sizeof(ch) / sizeof(ch[0]);
+//	bubble_sort(ch,sz);
 //	for (i = 0; i < 10; i++)
 //	{
 //		printf("%d ", ch[i]);
