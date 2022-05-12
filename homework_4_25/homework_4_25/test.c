139e094bbc0b991264f034a7f21120f0383f3c13
gascsd <3311922913@qq.com>
2022-05-12 19:10:50 +0800

    比特4月25日作业部分

---
+++ homework_4_25/homework_4_25/test.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//实现一个对整形数组的冒泡排序
+//#include<stdio.h>
+//void bubble_sort(int* arr)
+//{
+//	int tmp = 0;
+//	int i = 0;
+//	int count = 0;
+//	while (count < 10)
+//	{
+//		for (i = 0; i < 10 - count - 1; i++)
+//		{
+//			if (*(arr+i) > *(arr+i + 1))
+//			{
+//				tmp = *(arr + i);
+//				*(arr + i) = *(arr + i + 1);
+//				*(arr + i + 1) = tmp;
+//			}
+//		}
+//		count++;
+//	}
+//}
+//int main()
+//{
+//	int i = 0;
+//	int ch[10] = { 9,8,7,6,5,4,3,2,1,0 };
+//	bubble_sort(ch);
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", ch[i]);
+//	}
+//	return 0;
+//}
+
+
+//1. 完成三子棋游戏代码
+//2. 并将三子棋的设计和代码完成一篇博客。
