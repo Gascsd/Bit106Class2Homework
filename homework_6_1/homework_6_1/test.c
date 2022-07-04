38d17b57b043509d8fe75873d3d9ee4bf3d50ab4
gascsd <3311922913@qq.com>
2022-07-04 17:08:45 +0800

    比特6月1日作业补充修改

--- homework_6_1/homework_6_1/test.c
+++ homework_6_1/homework_6_1/test.c
@@ -1,4 +1,4 @@
-//#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_SECURE_NO_WARNINGS 1
 
 //写一个函数，判断一个字符串是否为另外一个字符串旋转之后的字符串。
 //例如：给定s1 = AABCD和s2 = BCDAA，返回1//AA BCDAA BCD
@@ -14,30 +14,15 @@
 //	char* p1, * p2;
 //	char s3[20] = { 0 };
 //	strcpy(s3, s1);
-//	char* tmp = s3;
-//	while (*tmp != '\0')
-//	{
-//		tmp++;
-//	}
-//	strcpy(tmp, s1);
-//	p1 = (char*)s3;
-//	p2 = s2;
-//	do
-//	{
-//		if (*p1 == *p2)
-//		{
-//			p2++;
-//		}
-//		p1++;
-//	} while (*p1 != '\0' && *p2 != '\0');
-//	if (*p2 == '\0')
+//	strcat(s3, s1);
+//	if (strstr(s3, s2))
 //		flag = 1;
 //	return flag;
 //}
 //int main()
 //{
-//	char s1[] = "ABCD";
-//	char s2[] = "abcd";
+//	char s1[] = "AABCD";
+//	char s2[] = "BCDAA";
 //	int length = strlen(s1);
 //	int ret = is_reserve(s1, s2);
 //	if (ret)
@@ -55,50 +40,160 @@
 
 
 //练习使用库函数，qsort排序各种类型的数据
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<math.h>
+//#include<string.h>
+//struct stu
+//{
+//	char name[20];
+//	int age;
+//};
+//int cmp_int(const void* e1, const void* e2)
+//{
+//	return *(int*)e1 - *(int*)e2;
+//}
+//int cmp_float(const void* e1, const void* e2)
+//{
+//	if ((*(float*)e1) < (*(float*)e2)
+//		&& fabs((*(float*)e1) - (*(float*)e2)) > 1e-6)
+//		return -1;
+//	else if ((*(float*)e1) > (*(float*)e2)
+//		&& fabs((*(float*)e1) - (*(float*)e2)) > 1e-6)
+//		return 1;
+//	else if (fabs((*(float*)e1) - (*(float*)e2)) < 1e-6)
+//		return 0;
+//}
+//int cmp_stu_by_name(const void* e1, const void* e2)
+//{
+//	return strcmp(((struct stu*)e1)->name, ((struct stu*)e2)->name);
+//}
+//int cmp_stu_by_age(const void* e1, const void* e2)
+//{
+//	return ((struct stu*)e1)->age - ((struct stu*)e2)->age;
+//}
+//int main()
+//{
+//	int i = 0;
+//	int arr1[5] = { 1,2,4,3,0 };
+//	float arr2[5] = { 1.5,5.5,20,5.25,8.5 };
+//	struct stu s[3] = { {"zhangsan",18},{"lisi",22},{"wangwu",20} };
+//
+//	qsort(arr1, 5, sizeof(int), cmp_int);
+//	printf("整型数据：");
+//	for (i = 0; i < 5; i++)
+//	{
+//		printf("%d ", arr1[i]);
+//	}
+//	printf("\n");
+//
+//	qsort(arr2, 5, sizeof(float), cmp_float);
+//	printf("浮点型数据数据：");
+//	for (i = 0; i < 5; i++)
+//	{
+//		printf("%.2f ", arr2[i]);
+//	}
+//	printf("\n");
+//
+//	qsort(s, 3, sizeof(struct stu), cmp_stu_by_name);
+//	printf("结构体按名字：\n");
+//	for (i = 0; i < 3; i++)
+//	{
+//		printf("%s %d\n", s[i].name, s[i].age);
+//	}
+//	printf("\n");
+//
+//	qsort(s, 3, sizeof(struct stu), cmp_stu_by_age);
+//	printf("结构体按年龄：\n");
+//	for (i = 0; i < 3; i++)
+//	{
+//		printf("%s %d\n", s[i].name, s[i].age);
+//	}
+//	printf("\n");
+//
+//	return 0;
+//}
+
+
+
+
+
+
+
+//模仿qsort的功能实现一个通用的冒泡排序
 #include<stdio.h>
-#include<stdlib.h>
-struct stu
+void Swap(char* e1,char* e2, int width)
 {
-	char name[20];
-	int age;
-};
+	int i = 0;
+	char tmp = 0;
+	for (i = 0; i < width; i++)
+	{
+		tmp = *(char*)e1;
+		*e1 = *e2;
+		*e2 = tmp;
+		e1++;
+		e2++;
+	}
+}
 int cmp_int(const void* e1, const void* e2)
 {
 	return *(int*)e1 - *(int*)e2;
 }
 int cmp_float(const void* e1, const void* e2)
 {
-
+	if ((*(float*)e1) < (*(float*)e2)
+		&& fabs((*(float*)e1) - (*(float*)e2)) > 1e-6)
+		return -1;
+	else if ((*(float*)e1) > (*(float*)e2)
+		&& fabs((*(float*)e1) - (*(float*)e2)) > 1e-6)
+		return 1;
+	else if (fabs((*(float*)e1) - (*(float*)e2)) < 1e-6)
+		return 0;
 }
-int cmp_stu_by_name(const void* e1, const void* e2)
-{
 
-}
-int cmp_stu_by_age(const void* e1, const void* e2)
+void bubble_sort(void* base, size_t num, size_t width, int(*cmp)(const void* e1, const void* e2))
 {
-
+	int i = 0;
+	int j = 0;
+	int flag = 1;
+	for (i = 0; i < num - 1; i++)
+	{
+		for (j = 0; j < num - i -1; j++)
+		{
+			if (cmp((char*)base + j * width, (char*)base + (j + 1) * width) > 0)
+			{
+				Swap((char*)base + j * width, (char*)base + (j + 1) * width, width);
+				flag = 0;
+			}
+		}
+		if (flag == 1)
+			break;
+	}
 }
 int main()
 {
 	int i = 0;
-	int arr1[5] = { 1,2,3,4,0 };
-	float arr2[5] = { 0.5,1.0,5,8.5,5.5 };
-	struct stu s[3] = { {"zhangsan",18},{"lisi",22},{"wangwu",20} };
-	qsort(arr1, 5, sizeof(int), cmp_int);
-	qsort(arr2, 5, sizeof(float), cmp_float);
-	qsort(s, 5, sizeof(struct stu), cmp_stu_by_name);
-	qsort(s, 5, sizeof(struct stu), cmp_stu_by_age);
+	int arr1[5] = { 1,2,4,3,0 };
+	float arr2[5] = { 1.5,5.5,20,5.25,8.5 };
+
+	bubble_sort(arr1, 5, sizeof(int), cmp_int);
+	printf("整型数据：");
 	for (i = 0; i < 5; i++)
 	{
 		printf("%d ", arr1[i]);
 	}
+	printf("\n");
+
+	bubble_sort(arr2, 5, sizeof(float), cmp_float);
+	printf("浮点型数据数据：");
+	for (i = 0; i < 5; i++)
+	{
+		printf("%.2f ", arr2[i]);
+	}
+	printf("\n");
+
 	return 0;
 }
-
-
-
-//模仿qsort的功能实现一个通用的冒泡排序
-
 
 
 
